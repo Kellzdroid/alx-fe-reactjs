a6f7bd41378b7d2494467649fc3a889d9ec8b081
Anderson Caleb <dmankellz@gmail.com>
2025-09-07 22:46:05 +0100

    My company

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./pages/Navbar";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
@@ -16,8 +17,9 @@ function App() {
       <div>
         <Router>
           <div>
-            <Navbar />
             <Routes>
+              <Navbar />
+              <Footer />
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/services" element={<Services />} />
