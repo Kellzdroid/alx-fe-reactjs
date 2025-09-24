f9aa76b285c0d7ee03ec786269a75dbc439c491a
Anderson Caleb <dmankellz@gmail.com>
2025-09-24 12:57:39 +0100

    Responsiveness added to card

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,15 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto, my-20 md:rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-8 md:max-w-sm sm:max-w-xs mx-auto sm:p-4 md:p-8 my-20 md:rounded-lg shadow-lg">
       <img
-        className="rounded-full w-36 h-36 mx-auto"
+        className="rounded-full md:w-36 md:h-36 mx-auto sm:w-24 sm:h-24  "
         src="https://via.placeholder.com/150"
         alt="User"
       />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">
+      <h1 className="text-xl text-blue-800 sm:text-lg md:text-xl my-4">
+        John Doe
+      </h1>
+      <p className="text-gray-600 md:text-base sm:text-sm ">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
