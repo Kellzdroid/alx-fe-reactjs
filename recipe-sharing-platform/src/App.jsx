4b78bbe9029009315cb18dc9c10191f035f5f990
Anderson Caleb <dmankellz@gmail.com>
2025-10-03 18:47:34 +0100

    First commit

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-yellow-600 bg-amber-900">
+          RECIPE SHARING PLATFORM
+        </h1>
+        <p className="bg-amber-950">Lorem ipsum dolor sit amet.</p>
+        <button className="bg-green-800 text-white rounded px-1.5 py-1 m-3 hover:bg-amber-400">
+          Click Me
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
